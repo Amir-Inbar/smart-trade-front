27db1d8d50961af916b797a5cbb63445f5a3653c
amirinbarcode@gmail.com <amirinbarcode@gmail.com>
2025-01-23 21:37:28 +0200

    fix: susponse

--- src/app/(smartTrade)/scenarios/page.tsx
+++ src/app/(smartTrade)/scenarios/page.tsx
@@ -1,43 +1,48 @@
 "use client";
 
 import Link from "next/link";
-import { lazy } from "react";
-import { ContentLayout } from "@/components/Layout/ContentLayout";
+import {lazy, Suspense} from "react";
+import {ContentLayout} from "@/components/Layout/ContentLayout";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
+
 const CreateScenarioModal = lazy(() => import("@/components/Scenarios/CreateScenarioModal/CreateScenarioModal"));
 const ScenariosOverview = lazy(() => import("@/components/Scenarios/ScenariosOverview/ScenariosOverview"));
 
 const Breadcrumbs = () => (
-  <Breadcrumb>
-    <BreadcrumbList>
-      <BreadcrumbItem>
-        <BreadcrumbLink asChild>
-          <Link href="/overview">Home</Link>
-        </BreadcrumbLink>
-      </BreadcrumbItem>
-      <BreadcrumbSeparator />
-      <BreadcrumbItem>
-        <BreadcrumbPage>CreateScenarioForm</BreadcrumbPage>
-      </BreadcrumbItem>
-    </BreadcrumbList>
-  </Breadcrumb>
+    <Breadcrumb>
+        <BreadcrumbList>
+            <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                    <Link href="/overview">Home</Link>
+                </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator/>
+            <BreadcrumbItem>
+                <BreadcrumbPage>CreateScenarioForm</BreadcrumbPage>
+            </BreadcrumbItem>
+        </BreadcrumbList>
+    </Breadcrumb>
 );
 
 const ScenariosPage = () => (
-  <ContentLayout title="CreateScenarioForm">
-    <Breadcrumbs />
-    <div className="m-3">
-      <CreateScenarioModal />
-    </div>
-    <ScenariosOverview />
-  </ContentLayout>
+    <ContentLayout title="CreateScenarioForm">
+        <Breadcrumbs/>
+        <div className="m-3">
+            <Suspense fallback={<div>Loading...</div>}>
+                <CreateScenarioModal/>
+            </Suspense>
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <ScenariosOverview/>
+        </Suspense>
+    </ContentLayout>
 );
 
 export default ScenariosPage;
