209a1755dbae86f1f8a717d011339779db36b5aa
amirinbarcode@gmail.com <amirinbarcode@gmail.com>
2025-02-18 14:46:40 +0200

    feat: fetch states on scenario in interval

--- src/components/Trades/TradesOverview/TradesOverview.util.tsx
+++ src/components/Trades/TradesOverview/TradesOverview.util.tsx
@@ -1,120 +1,117 @@
-import { MRT_Row } from "mantine-react-table";
-import { OrderStatusOptions, TradeSchema } from "@/schemas/types";
+import {MRT_Row} from "mantine-react-table";
+import {OrderStatusOptions, TradeSchema} from "@/schemas/types";
 
-import { IconClock, IconCheck, IconX, IconAlertTriangle } from "@tabler/icons-react";
-import { ReactElement } from "react"; // Assuming you're using Tabler Icons
+import {IconClock, IconCheck, IconX, IconAlertTriangle} from "@tabler/icons-react";
+import {ReactElement} from "react"; // Assuming you're using Tabler Icons
 
 export const OrderStatusToConfig: {
-  [key in OrderStatusOptions]: {
-    label: string;
-    color: string;
-    icon: ReactElement;
-  };
+    [key in OrderStatusOptions]: {
+        label: string;
+        color: string;
+        icon: ReactElement;
+    };
 } = {
-  [OrderStatusOptions.PENDING]: {
-    label: "Pending",
-    color: "text-blue-500",
-    icon: <IconClock size={16} className="text-blue-500" />
-  },
-  [OrderStatusOptions.FILLED]: {
-    label: "Filled",
-    color: "text-green-500",
-    icon: <IconCheck size={16} className="text-green-500" />
-  },
-  [OrderStatusOptions.CANCELLED]: {
-    label: "Cancelled",
-    color: "text-red-500",
-    icon: <IconX size={16} className="text-red-500" />
-  },
-  [OrderStatusOptions.ERROR]: {
-    label: "Error",
-    color: "text-yellow-500",
-    icon: <IconAlertTriangle size={16} className="text-yellow-500" />
-  }
+    [OrderStatusOptions.PENDING]: {
+        label: "Pending",
+        color: "text-blue-500",
+        icon: <IconClock size={16} className="text-blue-500"/>
+    },
+    [OrderStatusOptions.FILLED]: {
+        label: "Filled",
+        color: "text-green-500",
+        icon: <IconCheck size={16} className="text-green-500"/>
+    },
+    [OrderStatusOptions.CANCELLED]: {
+        label: "Cancelled",
+        color: "text-red-500",
+        icon: <IconX size={16} className="text-red-500"/>
+    },
+    [OrderStatusOptions.ERROR]: {
+        label: "Error",
+        color: "text-yellow-500",
+        icon: <IconAlertTriangle size={16} className="text-yellow-500"/>
+    }
 };
 
 
 export const getTradesColumns = () => [
-  {
-    accessorKey: "scenario.id",
-    header: "Scenario ID",
-    size: 100
-  },
-  {
-    accessorKey: "contract",
-    header: "Contract Name",
-    size: 30,
-    Cell: ({ row }: { row: MRT_Row<TradeSchema> }) => {
-      const contractName = row.original.contract?.name ?? "No contract name";
-      return <span>{contractName}</span>;
-    }
-  },
-  {
-    accessorKey: "orders",
-    header: "Orders",
-    Cell: ({ row }: { row: MRT_Row<TradeSchema> }) => {
-      const orders = row.original.orders ?? [];
-      if (orders.length === 0) {
-        return <div>No orders available</div>;
-      }
-      return (
-        <div>
-          {orders.map((order) => order.status ? (
-            <div key={order.id} className="flex justify-between items-center py-2">
-              <div>
-                <strong>Action:</strong> {order.action},
-                <strong> Quantity:</strong> {order.quantity},
-                <strong> Type:</strong> {order.order_type},
-                <strong> Price:</strong> {order.lmt_price},
-                <strong> Broker ID:</strong> {order.broker_order_id}
-              </div>
+    {
+        accessorKey: "scenario.id",
+        header: "Scenario ID",
+        size: 100
+    },
+    {
+        accessorKey: "contract",
+        header: "Contract Name",
+        size: 30,
+        Cell: ({row}: { row: MRT_Row<TradeSchema> }) => {
+            const contractName = row.original.contract?.name ?? "No contract name";
+            return <span>{contractName}</span>;
+        }
+    },
+    {
+        accessorKey: "orders",
+        header: "Orders",
+        Cell: ({row}: { row: MRT_Row<TradeSchema> }) => {
+            const orders = row.original.orders ?? [];
+            if (orders.length === 0) {
+                return <div>No orders available</div>;
+            }
 
-              <div className="text-right flex items-center">
-                {OrderStatusToConfig[order.status] ? (
-                  <span
-                    className={`text-lg font-semibold flex items-center ${OrderStatusToConfig[order.status].color}`}>
-                  <span>
-                    {OrderStatusToConfig[order.status].icon}
-                  </span>
-                    <span className="ml-2">
-                      {OrderStatusToConfig[order.status].label}
-                    </span>
-                </span>
-                ) : (
-                  <span className="text-lg font-semibold text-gray-500">Unknown Status</span>
-                )}
-              </div>
-            </div>
-          ) : null)}
-        </div>
-      );
+            const entryOrder = orders.find((o) => o.action === "BUY");
+            const stopLossOrder = orders.find((o) => o.action === "SELL" && o.order_type === "STP");
+            const takeProfitOrders = orders.filter((o) => o.action === "SELL" && o.order_type === "LMT");
+
+            return (
+                <div className="space-y-2 w-1/3">
+                    <div>
+                        <strong>Action | Quantity | Price</strong>
+                    </div>
+                    {entryOrder && (
+                        <div className="border p-2 rounded text-blue-100 ׳">
+                            <strong>Entry:</strong> {entryOrder.quantity} | {entryOrder.lmt_price}
+                        </div>
+                    )}
+                    {stopLossOrder && (
+                        <div className="border p-2 rounded text-red-100">
+                            <strong>Stop Loss:</strong> {stopLossOrder.quantity} | {stopLossOrder.lmt_price}
+                        </div>
+                    )}
+                    {takeProfitOrders.map((order, index) => (
+                        <div key={order.id} className="border p-2 rounded text-green-100">
+                            <strong>Take Profit {index + 1}:</strong> {order.quantity} | {order.lmt_price}
+                        </div>
+                    ))}
+                </div>
+            );
+        }
     }
-  }
 ];
 
+
 export const TradesOverviewDataInitialState = () => ({
-  enableGrouping: true,
-  enableStickyHeader: true,
-  enableTopToolbar: true,
-  enableStickyFooter: true,
-  enableColumnResizing: true,
-  enableRowSelection: false,
-  editDisplayMode: "modal",
-  enableFullScreenToggle: true,
-  enableHiding: true,
-  paginationDisplayMode: "pages",
-  initialState: {
-    grouping: ["scenario.id"],
-    density: "xs",
-    expanded: true,
-    pagination: { pageIndex: 0, pageSize: 20 },
-    columnVisibility: {
-      id: false,
-      contract_id: false,
-      stop_price_mode: false,
-      description: false,
-      created_at: false,
-      updated_at: false
+    enableGrouping: true,
+    enableStickyHeader: true,
+    enableTopToolbar: true,
+    enableStickyFooter: true,
+    enableColumnResizing: true,
+    enableRowSelection: false,
+    editDisplayMode: "modal",
+    enableFullScreenToggle: true,
+    enableHiding: true,
+    paginationDisplayMode: "pages",
+    initialState: {
+        grouping: ["scenario.id"],
+        density: "xs",
+        expanded: true,
+        pagination: {pageIndex: 0, pageSize: 20},
+        columnVisibility: {
+            id: false,
+            contract_id: false,
+            stop_price_mode: false,
+            description: false,
+            created_at: false,
+            updated_at: false
+        }
     }
-  }
 });
