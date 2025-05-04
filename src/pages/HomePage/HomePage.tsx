d711b54c8f3625c3a775eb75569f8cf3807d5cbb
amirinbarcode@gmail.com <amirinbarcode@gmail.com>
2025-05-04 22:26:11 +0300

    feat: adds routes to services status

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,12 @@
-import {useFetchAccountNumberQuery} from "../../store/api/accountApi.ts";
-import {ContentLayout} from "../../components/Layout/ContentLayout.tsx";
+import {ContentLayout} from "../../components/Layout/ContentLayout";
 
 const HomePage = () => {
-    const {data: accountNumber} = useFetchAccountNumberQuery();
 
     return (
         <ContentLayout title="Home">
             <h1 className="text-2xl pb-2 font-bold">Overview</h1>
             <p className="text-sm pb-2 text-gray-500">
-                Welcome back, <span className="font-bold">{accountNumber}</span>
+                Welcome back!
             </p>
         </ContentLayout>
     );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,11 @@ import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import {useDispatch} from "react-redux";
 import {scenarioApi} from "./api/scenarioApi";
 import {contractApi} from "./api/contractApi";
-import {accountApi} from "./api/accountApi";
 import {tradeApi} from "./api/tradeApi";
 import {dailyTradeEventsApi} from "@/store/api/dailyTradeEventsApi";
 
 const rootReducer = combineReducers({
     [scenarioApi.reducerPath]: scenarioApi.reducer,
-    [accountApi.reducerPath]: accountApi.reducer,
     [contractApi.reducerPath]: contractApi.reducer,
     [tradeApi.reducerPath]: tradeApi.reducer,
     [dailyTradeEventsApi.reducerPath]: dailyTradeEventsApi.reducer,
@@ -19,7 +17,6 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
             scenarioApi.middleware,
-            accountApi.middleware,
             contractApi.middleware,
             tradeApi.middleware,
             dailyTradeEventsApi.middleware,
